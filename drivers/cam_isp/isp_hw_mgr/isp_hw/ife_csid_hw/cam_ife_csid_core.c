843495af8dc9092dff615d2fbe406da042c09abb
Karthik Jayakumar <kjayakum@codeaurora.org>
2019-11-05 16:53:31 -0800

    msm: camera: config: Makefile fixes to support legacy kernel build system
    Adds fixes to makefile in order to support legacy kernel build systems.
    
    CRs-Fixed: 3280235
    Change-Id: Iab571871e5171aab501c41496cc09e3c5d942985
    Signed-off-by: Karthik Jayakumar <kjayakum@codeaurora.org>
    Signed-off-by: Nirmal Abraham <quic_c_nabrah@quicinc.com>

--- drivers/cam_isp/isp_hw_mgr/isp_hw/ife_csid_hw/cam_ife_csid_core.c
+++ drivers/cam_isp/isp_hw_mgr/isp_hw/ife_csid_hw/cam_ife_csid_core.c
@@ -5,8 +5,8 @@
 
 #include <linux/iopoll.h>
 #include <linux/slab.h>
-#include <uapi/media/cam_isp.h>
-#include <uapi/media/cam_defs.h>
+#include <media/cam_isp.h>
+#include <media/cam_defs.h>
 #include <media/cam_req_mgr.h>
 #include <dt-bindings/msm/msm-camera.h>
 
