7504908856d641bd4a2f2f0ccc3aff7cd5056a3e
chengxue <quic_chengxue@quicinc.com>
2022-06-24 16:55:16 +0800

    msm: camera: jpeg: Increase the max context number to 24
    Customer may have more than 3 physical cameras, and userland
    may also create multi jpeg pipelines, so increase the max
    context number to 24.
    
    CRs-Fixed: 3228532
    Change-Id: Ia90947f03a1543f19684aa7cad1b1972419e9c74
    Signed-off-by: chengxue <quic_chengxue@quicinc.com>

--- drivers/cam_jpeg/jpeg_hw/include/cam_jpeg_hw_mgr_intf.h
+++ drivers/cam_jpeg/jpeg_hw/include/cam_jpeg_hw_mgr_intf.h
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
  * Copyright (c) 2017-2021, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #ifndef CAM_JPEG_HW_MGR_INTF_H
@@ -10,7 +11,7 @@
 #include <media/cam_jpeg.h>
 #include <media/cam_defs.h>
 
-#define CAM_JPEG_CTX_MAX        16
+#define CAM_JPEG_CTX_MAX        24
 
 enum cam_jpeg_hw_type {
 	CAM_JPEG_DEV_ENC,
