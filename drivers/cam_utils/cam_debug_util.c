afaad38b36a20af714551e9fb2151054624235a1
Pavan Kumar Chilamkurthi <quic_pchilamk@quicinc.com>
2022-11-03 12:48:38 -0700

    msm: camera: utils: Fix compilation error due to missing param
    With more restricted compilation flags, void parameter is
    needed in funciton prototype if no other args exist.
    
    CRs-Fixed: 3306362
    Change-Id: I7cdc7d56ff0a5bf5e626ff964dd284faae424bc1
    Signed-off-by: Pavan Kumar Chilamkurthi <quic_pchilamk@quicinc.com>

--- drivers/cam_utils/cam_debug_util.c
+++ drivers/cam_utils/cam_debug_util.c
@@ -106,7 +106,7 @@ int cam_debugfs_lookup_subdir(const char *name, struct dentry **subdir)
 	return (*subdir) ? 0 : -ENOENT;
 }
 
-const struct camera_debug_settings *cam_debug_get_settings()
+const struct camera_debug_settings *cam_debug_get_settings(void)
 {
 	return &cam_debug;
 }
