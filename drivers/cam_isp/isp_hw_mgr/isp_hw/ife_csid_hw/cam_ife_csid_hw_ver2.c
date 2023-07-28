51846a0b77b73a2b24e45c05bd45a312cbd81e36
Karthik Anantha Ram <quic_kartanan@quicinc.com>
2023-07-28 14:02:25 -0700

    msm: camera: isp: Fix reset sequence for dual case
    There is no need to explicitly reset the slave CSID,
    issuing a reset to master (both HW & SW) will be propagated
    to the slave CSID. This avoids the handling of additional
    reset_ack irq from slave, and any associated handling.
    
    CRs-Fixed: 3580847
    Change-Id: I9f787619c8322b7c3f96430762f4197af19e51fd
    Signed-off-by: Karthik Anantha Ram <quic_kartanan@quicinc.com>
    (cherry picked from commit 7356076769c8f381f6980f26e7a12f06a66af268)

--- drivers/cam_isp/isp_hw_mgr/isp_hw/ife_csid_hw/cam_ife_csid_hw_ver2.c
+++ drivers/cam_isp/isp_hw_mgr/isp_hw/ife_csid_hw/cam_ife_csid_hw_ver2.c
@@ -2516,14 +2516,12 @@ static int cam_ife_csid_ver2_internal_reset(
 		cam_io_w_mb(0x0, mem_base + csi2_reg->cfg1_addr);
 	}
 
-	if (csid_hw->sync_mode == CAM_ISP_HW_SYNC_SLAVE &&
-		rst_cmd == CAM_IFE_CSID_RESET_CMD_HW_RST)
+	if (csid_hw->sync_mode == CAM_ISP_HW_SYNC_SLAVE)
 		goto wait_only;
 
-	if (rst_cmd == CAM_IFE_CSID_RESET_CMD_SW_RST)
-		reinit_completion(&csid_hw->hw_info->hw_complete);
+	reinit_completion(&csid_hw->hw_info->hw_complete);
 
-	/*Program the reset location */
+	/* Program the reset location */
 	if (rst_location == CAM_IFE_CSID_RESET_LOC_PATH_ONLY)
 		val |= (csid_reg->cmn_reg->rst_loc_path_only_val <<
 		       csid_reg->cmn_reg->rst_location_shift_val);
