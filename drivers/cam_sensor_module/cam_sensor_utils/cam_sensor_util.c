11d0eea369f4c0c18a9c89449f1a8ac18fd177f7
Yulei Yao <quic_yuleiy@quicinc.com>
2023-09-01 15:41:19 -0700

    msm: camera: sensor: Fix delay pass issue in I2C sequential write
    When I2C sequential write is applied, first register's delay parameter
    is used instead of the last one in the sequential queue.
    
    CRs-Fixed: 3566036
    External Impact: No.
    
    Change-Id: I0aa6480d7b577fdb697bc28b61aed428d5cae8fb
    Signed-off-by: Yulei Yao <quic_yuleiy@quicinc.com>

--- drivers/cam_sensor_module/cam_sensor_utils/cam_sensor_util.c
+++ drivers/cam_sensor_module/cam_sensor_utils/cam_sensor_util.c
@@ -205,9 +205,15 @@ int32_t cam_sensor_handle_delay(
 		i2c_list =
 			list_entry(list_ptr, struct i2c_settings_list, list);
 		if (generic_op_code ==
-			CAMERA_SENSOR_WAIT_OP_HW_UCND)
-			i2c_list->i2c_settings.reg_setting[offset - 1].delay =
-				cmd_uncond_wait->delay;
+			CAMERA_SENSOR_WAIT_OP_HW_UCND) {
+			int32_t size = i2c_list->i2c_settings.size;
+
+			if (offset >= size)
+				i2c_list->i2c_settings.reg_setting[offset - size].delay =
+					cmd_uncond_wait->delay;
+			else
+				CAM_WARN(CAM_SENSOR_UTIL, "Setting size is bigger than offset.");
+		}
 		else
 			i2c_list->i2c_settings.delay = cmd_uncond_wait->delay;
 		(*cmd_buf) +=
